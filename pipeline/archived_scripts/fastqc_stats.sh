b6e5c8a6583a5d92a2a1c4cdd4b2b10ce53c8fc8
cednotsed <cscstan@CZS1JR3.thecrick.org>
2023-02-22 15:40:47 +0000

    publication cleanup

--- pipeline/archived_scripts/fastqc_stats.sh
+++
@@ -1,7 +0,0 @@
-for file in parsed_fastq/*.fastq
-do
-    fastqc ${file} -o ./qc_files
-
-done
-
-multiqc ./qc_files
--- simulation_pipeline/align_to_human_ref.sh
+++
@@ -1,5 +0,0 @@
-prefix=10e-5
-data=/home/projects/14001280/PROJECTS/blood_microbiome/data/simulation_references
-bowtie2 -x ${data}/human/ref -1 ${data}/simulated_reads/simulated.${prefix}.1.fastq \
-    -2 ${data}/simulated_reads/simulated.${prefix}.2.fastq | samtools view -C -T ${data}/human/GRCh38_latest_genomic.fna > ${data}/simulated_reads/simulated.${prefix}.cram
-
